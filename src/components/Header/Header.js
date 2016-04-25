8982c06e9d714863f3a0fad4f038c85a4e5c295e
febobo <febobo@zhangbodeMacBook-Pro.local>
2016-04-25 14:47:44 +0800

    init

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,18 +1,42 @@
 import React from 'react'
 import { IndexLink, Link } from 'react-router'
 import classes from './Header.scss'
+import logoImg from '../../static/images/logo.png'
+import arrowIco from '../../static/images/arrowIco.png'
 
 export const Header = () => (
-  <div>
-    <h1>React Redux Starter Kit</h1>
-    <IndexLink to='/' activeClassName={classes.activeRoute}>
-      Home
-    </IndexLink>
-    {' · '}
-    <Link to='/counter' activeClassName={classes.activeRoute}>
-      Counter
-    </Link>
+
+  <div className={classes.topBg}>
+  	<div className={classes.top}>
+  		<div className={classes.logo}>
+
+        <a href="/"><img src={logoImg} /></a>
+      </div>
+  		<div className={classes.language}>
+  			<span>中文简体<em>|</em></span>
+  			<a href="#"><img src={arrowIco} className={classes.arrowIco} /></a>
+  		</div>
+  		<div className={classes.oline}>
+  			<strong></strong>
+  			<span>Oline：<em>1357</em><span>
+  		</span></span></div>
+  	</div>
   </div>
+
+
 )
 
 export default Header
+
+        // <a href="/"><img src={require('./logo.png')}></a>
+// <div>
+//   <h1>React Redux Starter Kit</h1>
+//   <IndexLink to='/' activeClassName={classes.activeRoute}>
+//     Home
+//   </IndexLink>
+//   {' · '}
+//   <Link to='/counter' activeClassName={classes.activeRoute}>
+//     Counter
+//   </Link>
+//
+// </div>
