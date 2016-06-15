02ae2dc8fa29c7931d87a83136a1fc732db2e65a
“GIT <584516601@qq.com>
2016-06-16 01:55:04 +0800

    config domain

--- src/BaseConfig/index.js
+++ src/BaseConfig/index.js
@@ -1,12 +1,12 @@
 const apiArr = [
-  'https://solebtc.com/api/v1',
+  'https://offerwall.solebtc.com/api/v1',
   'https://soleltc.com/api/v1',
   'https://soledash.net/api/v1',
   'https://soledoges.com/api/v1',
 ]
 
 const socketApiArr = [
-  'wss://solebtc.com/api/v1/websocket',
+  'wss://offerwall.solebtc.com/api/v1/websocket',
   'wss://soleltc.com/api/v1/websocket',
   'wss://soledash.net/api/v1/websocket',
   'wss://soledoges.com/api/v1/websocket',
